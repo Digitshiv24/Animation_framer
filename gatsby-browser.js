498e9174e4862c11036262c53a30b1975d246f67
Shivam Sharma <you@example.com>
2023-02-28 12:51:27 +0530

    fix-buils issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,7 @@
 // You can delete this file if you're not using it
 
 import React from "react";
-import Layout from './src/Components/Layout/Layout'
+import Layout from './src/Components/Layout/Layout.js'
 
 export const wrapPageElement = ({ element }) => {
   return (
